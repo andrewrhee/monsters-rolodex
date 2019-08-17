4be6253d048571d4ad30d466ef0ed484bc28f599
Andrew Rhee <adrhee@gmail.com>
2019-08-17 13:50:43 -0700

    Updates

---
+++ precache-manifest.a3f07dd1240d46fb3108d0bd9b79721d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6680125dc464210f4110170a4dcd289f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f2f1f7a62090a0a588c4",
+    "url": "/monsters-rolodex/static/css/main.77014e1a.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "f2f1f7a62090a0a588c4",
+    "url": "/monsters-rolodex/static/js/main.4a695811.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
